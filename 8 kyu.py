f685f7d2d97fba137b6b115fb365e2137ea8e171
aleqsandreapkhazava <161210036+aleqsandreapkhazava@users.noreply.github.com>
2024-08-23 16:11:50 +0400

    Add files via upload

---
+++ 8 kyu.py
@@ -0,0 +1,23 @@
+def get_real_floor(n):
+    if (n) > 13:
+        return (n)- 2
+    elif (n) == 1:
+        return 0
+    elif (n) == 5:
+        return 4
+    elif (n) < 0:
+        return (n)
+    elif (n) == 0:
+        return 0
+    elif (n) == 10:
+        return 9
+    elif (n) == 12:
+        return 11
+    elif (n) > 0:
+        return (n) -1
+    
+        
+    else:
+        return (n)
+    
+    
